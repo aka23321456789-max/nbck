d1ad65635e2793ed05d8b359bcd69be579f47e18
aka23321456789-max <aka23321456789@gmail.com>
2026-03-10 12:57:24 +0800

    feat(test): 添加新的测试打印语句
    - 在TestPrint类中添加了"Hello, 123456789!"的打印语句
    - 扩展了测试输出内容以验证更多功能场景

--- new001/src/main/java/com/itheima/TestPrint.java
+++ new001/src/main/java/com/itheima/TestPrint.java
@@ -4,5 +4,6 @@ public class TestPrint {
     public static void main(String[] args) {
         System.out.println("你好，这是一个测试打印！");
         System.out.println("Hello, this is a test print!");
+        System.out.println("Hello, 123456789!");
     }
 }
