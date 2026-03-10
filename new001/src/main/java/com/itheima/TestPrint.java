c105634bf39da302ff31835b6e3b6e61b65fdee7
aka23321456789-max <aka23321456789@gmail.com>
2026-03-10 12:55:56 +0800

    update
    update

---
+++ new001/src/main/java/com/itheima/TestPrint.java
@@ -0,0 +1,8 @@
+package com.itheima;
+
+public class TestPrint {
+    public static void main(String[] args) {
+        System.out.println("你好，这是一个测试打印！");
+        System.out.println("Hello, this is a test print!");
+    }
+}
---
+++ new001/src/test/java/com/itheima/new001/New001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.new001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class New001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
